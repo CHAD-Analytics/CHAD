509b8a67f55420edcc4ac28e210aa20741021793
Trey Pujats <tpujats@gmail.com>
2020-04-14 22:11:36 -0400

    Redeploy

--- global.R
+++ global.R
@@ -2764,18 +2764,18 @@ GetHeatMap<-function(MAJCOMChoice,ModelChoice,ForecastChoice,Stat){
       countrycolor = toRGB("white")
     )
     
-    fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(50, 400))
+    fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(20, 400))
     fig <- fig %>% add_markers(
       x = ~Long, y = ~Lat, size = ~IHME, color = ~IHMEID, colors = c("red","#228B22"), hoverinfo = "text",
       text = ~paste(HeatMap$Base, "<br />", HeatMap$IHME)
     )
     fig <- fig %>% layout(title = Banner , geo = g, showlegend=TRUE)
     
-    # legend.sizes = seq(50,400, 50)
+    # legend.sizes = seq(0,max(HeatMap$IHME), round(max(HeatMap$IHME)/8, -1))
     # ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
     # mk = list(sizeref=0.1, sizemode="area")
     # p.legend = plot_ly() %>%
-    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk) %>%
+    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk,  color = "#228B22") %>%
     #   layout(xaxis = ax, yaxis = list(showgrid = FALSE))
     # 
     # subplot(p.legend, fig, widths = c(0.1, 0.9))
@@ -2794,18 +2794,18 @@ GetHeatMap<-function(MAJCOMChoice,ModelChoice,ForecastChoice,Stat){
       countrycolor = toRGB("white")
     )
     
-    fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(20, 300))
+    fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(20, 400))
     fig <- fig %>% add_markers(
       x = ~Long, y = ~Lat, size = ~CHIME, color = ~CHIMEID, colors = c("red","#228B22"), hoverinfo = "text",
       text = ~paste(HeatMap$Base, "<br />", HeatMap$CHIME)
     )
     fig <- fig %>% layout(title = Banner , geo = g,showlegend=TRUE)
     
-    # legend.sizes = seq(50,300, 50)
+    # legend.sizes = seq(0,max(HeatMap$CHIME), round(max(HeatMap$CHIME)/8, -1))
     # ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
     # mk = list(sizeref=0.1, sizemode="area")
     # p.legend = plot_ly() %>%
-    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk) %>%
+    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk, color = "#228B22" ) %>%
     #   layout(xaxis = ax, yaxis = list(showgrid = FALSE))
     # 
     # subplot(p.legend, fig, widths = c(0.1, 0.9))
