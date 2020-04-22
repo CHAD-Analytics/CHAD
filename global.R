8b35c79f282f7aeef240f33b22273dce0dd9da15
galarcon0308 <52179539+galarcon0308@users.noreply.github.com>
2020-04-22 13:11:51 -0400

    Change to cases per 1000

--- global.R
+++ global.R
@@ -485,7 +485,7 @@ CalculateCovid1000<-function(IncludedCounties){
   
   #Get total confirmed cases in the selected region
   CovidCounties<-subset(CovidConfirmedCases, CountyFIPS %in% IncludedCounties$FIPS)
-  (sum(rev(CovidCounties)[,1]))/(sum(IncludedCounties$Population))*10000
+  ceiling((sum(rev(CovidCounties)[,1]))/(sum(IncludedCounties$Population))*1000)
 }
 
 CalculateDeaths<-function(IncludedCounties){
