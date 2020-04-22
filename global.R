618a7720283f035f9c6de50f126ff9e502379e7b
tleonard6 <taylorleonard15@gmail.com>
2020-04-22 14:38:17 -0400

    Caned to cases per 1000 to match Garrett's last commit

--- global.R
+++ global.R
@@ -520,7 +520,7 @@ CalculateCovid1000<-function(IncludedCounties){
   
   #Get total confirmed cases in the selected region
   CovidCounties<-subset(CovidConfirmedCases, CountyFIPS %in% IncludedCounties$FIPS)
-  (sum(rev(CovidCounties)[,1]))/(sum(IncludedCounties$Population))*10000
+  (sum(rev(CovidCounties)[,1]))/(sum(IncludedCounties$Population))*1000
 }
 
 CalculateDeaths<-function(IncludedCounties){
