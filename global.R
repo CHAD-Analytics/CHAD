bf3baff54c80c7e37ea9434b1beef85e4ca27421
ltcarendt <59400008+ltcarendt@users.noreply.github.com>
2020-04-25 14:12:59 -0400

    Update global.R
    Corrected variable name for temporary shaman.lab.path

--- global.R
+++ global.R
@@ -80,9 +80,9 @@ if (test_date < Sys.Date()) {
   shaman.lab.path = "Projection_April23"
   for (k in 14:0) {
     if (lubridate::day(as.Date(Sys.Date()-k))>9) {
-      my_path = paste0("Projection_",lubridate::month(as.Date(Sys.Date()-k),label=TRUE,abbr=FALSE),lubridate::day(as.Date(Sys.Date()-k)))
+      my_shaman_lab_path = paste0("Projection_",lubridate::month(as.Date(Sys.Date()-k),label=TRUE,abbr=FALSE),lubridate::day(as.Date(Sys.Date()-k)))
     } else {
-      my_path = paste0("Projection_",lubridate::month(as.Date(Sys.Date()-k),label=TRUE,abbr=FALSE),"0",lubridate::day(as.Date(Sys.Date()-k)))
+      my_shaman_lab_path = paste0("Projection_",lubridate::month(as.Date(Sys.Date()-k),label=TRUE,abbr=FALSE),"0",lubridate::day(as.Date(Sys.Date()-k)))
     }
     if (max(grepl(my_shaman_lab_path,shaman.lab.json$path,fixed=TRUE))) {
       shaman.lab.path = my_shaman_lab_path
