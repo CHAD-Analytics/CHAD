c1d5ca3c3624cb9d630b024dab873758eeb7c71a
Andrew Keith <keith.andrew.j@gmail.com>
2020-04-27 11:14:19 -0400

    Update ui.R

--- covid19/ui.R
+++ covid19/ui.R
@@ -180,7 +180,7 @@ ui <- tagList(
                     '))),
                       tags$script(HTML('
                                    $(document).ready(function() {
-                                   $("header").find("nav").append(\'<span class="myClass"> COVID-19 Health Assessment Dashboard Beta v0.6</span>\');
+                                   $("header").find("nav").append(\'<span class="myClass"> COVID-19 Health Assessment Dashboard Beta v0.8.1</span>\');
                                    })
                                    ')),
                       tabsetPanel(id = "tabselected",
