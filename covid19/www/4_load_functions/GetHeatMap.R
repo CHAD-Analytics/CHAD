de74f7c1814c2ba710a213e392dd0b343a71d0ad
courtneylknoth <courtney.l.knoth.civ@mail.mil>
2020-04-30 16:05:11 -0400

    Update GetHeatMap.R
    -updated legend, colors, and sizes

--- covid19/www/4_load_functions/GetHeatMap.R
+++ covid19/www/4_load_functions/GetHeatMap.R
@@ -31,25 +31,31 @@ GetHeatMap<-function(MAJCOMChoice,ModelChoice,ForecastChoice,Stat){
       countrycolor = toRGB("white")
     )
     
+    legend.sizes = seq(min(HeatMap$IHME),max(HeatMap$IHME), round(max(HeatMap$IHME)/8, -1))
+    ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
+    mk = list(sizeref=0.15, sizemode="area", symbol="circle-open", color="black")
+     
     fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(20, 400))
-    fig <- fig %>% add_markers(
-      x = ~Long, y = ~Lat, size = ~IHME, color = ~IHMEID, colors = c("red","#228B22"), hoverinfo = "text",
-      text = ~paste(HeatMap$Base, "<br />", HeatMap$IHME)
+    fig <- fig %>% add_markers(x = ~Long, y = ~Lat, size = ~IHME, color = ~IHMEID, colors= c("Over 5% Population"="red", 
+      "Under 5% Population"="#228B22",
+      "Over Capacity"="red", "Under Capacity"="#228B22"), hoverinfo = "text",
+      text = ~paste(HeatMap$Base, "<br />", HeatMap$IHME), marker=list(sizeref=0.55, opacity=0.5, sizemode="area")
     )
+    
     fig <- fig %>% layout(title = Banner , geo = g, showlegend=TRUE)
     fig <- fig %>% layout(legend = list(orientation = "h",   # show entries horizontally
                                         xanchor = "center",  # use center of legend as anchor
                                         x = 0.5,
                                         y = 0.95))
     
-    # legend.sizes = seq(20,max(HeatMap$IHME), round(max(HeatMap$IHME)/8, -1))
-    # ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
-    # mk = list(sizeref=0.1, sizemode="area")
-    # p.legend = plot_ly() %>%
-    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk,  color = "#228B22") %>%
-    #   layout(xaxis = ax, yaxis = list(showgrid = FALSE))
-    # 
-    # subplot(p.legend, fig, widths = c(0.1, 0.9))
+
+    p.legend = plot_ly() %>%
+      add_markers(x=1, y=legend.sizes, size = legend.sizes, showlegend = F, marker=mk,hoverinfo="text",
+                  text= ~paste("Frequency = ", legend.sizes)) %>%
+      layout(xaxis = ax, yaxis = list(showgrid = FALSE, zeroline=FALSE, showline=FALSE))
+
+    fig<-subplot(p.legend, fig, widths = c(0.1, 0.9))
+    
     fig
     
   } else {
@@ -64,29 +70,39 @@ GetHeatMap<-function(MAJCOMChoice,ModelChoice,ForecastChoice,Stat){
       subunitcolor = toRGB("white"),
       countrycolor = toRGB("white")
     )
+
+    legend.sizes = seq(min(HeatMap$CHIME), max(HeatMap$CHIME), round(max(HeatMap$CHIME)/8,-2))  
+    ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
+    mk = list(sizeref=0.15, sizemode="area", symbol="circle-open", color="black") 
     
     fig <- plot_geo(HeatMap, locationmode = 'USA-states', sizes = c(20, 400))
-    fig <- fig %>% add_markers(
-      x = ~Long, y = ~Lat, size = ~CHIME, color = ~CHIMEID, colors = c("red","#228B22"), hoverinfo = "text",
-      text = ~paste(HeatMap$Base, "<br />", HeatMap$CHIME)
+    fig <- fig %>% add_markers(x = ~Long, y = ~Lat, size =~CHIME,  
+      color = ~CHIMEID, colors= c("Over 5% Population"="red", 
+      "Under 5% Population"="#228B22",
+     "Over Capacity"="red", "Under Capacity"="#228B22"), hoverinfo = "text",
+      text = ~paste(HeatMap$Base, "<br />", HeatMap$CHIME), marker=list(sizeref=0.55, opacity=0.5, sizemode="area")
     )
+    
     fig <- fig %>% layout(title = Banner , geo = g,showlegend=TRUE)
     fig <- fig %>% layout(legend = list(orientation = "h",   # show entries horizontally
-                                        xanchor = "center",  # use center of legend as anchor
-                                        x = 0.5,
-                                        y = 0.97))
+                                         xanchor = "center",  # use center of legend as anchor
+                                         x = 0.5,
+                                         y = 0.95))
     
-    # legend.sizes = seq(0,max(HeatMap$CHIME), round(max(HeatMap$CHIME)/8, -1))
-    # ax = list(zeroline = FALSE, showline = FALSE, showticklabels = FALSE, showgrid = FALSE)
-    # mk = list(sizeref=0.1, sizemode="area")
-    # p.legend = plot_ly() %>%
-    #   add_markers(x = 1, y = legend.sizes, size = legend.sizes, showlegend = F, marker = mk, color = "#228B22" ) %>%
-    #   layout(xaxis = ax, yaxis = list(showgrid = FALSE))
-    # 
-    # subplot(p.legend, fig, widths = c(0.1, 0.9))
     
+
+    p.legend = plot_ly() %>%
+      add_markers(x=1, y=legend.sizes, size =legend.sizes, showlegend=F, marker=mk,hoverinfo="text",
+                  text= ~paste("Frequency = ", legend.sizes)) %>% 
+      layout(xaxis = ax,
+             yaxis=list(showgrid=FALSE, zeroline=FALSE, showline=FALSE))
+
+    fig<-subplot(p.legend, fig, widths = c(0.1, 0.9))
+
     fig
     
   }
   
+
+
 }
