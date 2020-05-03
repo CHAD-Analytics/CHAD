ff3018c0c50de2ad6dd68199805452d8fb26e975
Garrett <inferno298@msn.com>
2020-05-03 14:32:29 -0400

    Merge OCONUS Data

--- covid19/www/3_load_external_data/09_NationalDataTable.R
+++ covid19/www/3_load_external_data/09_NationalDataTable.R
@@ -1,9 +1,26 @@
 #' Create National Data table on summary page
 #' 
+
+StatePop = CountyInfo
+StatePop$State = as.factor(StatePop$State)
+StatePop = StatePop[,c("State","Population")]
+StatePop = aggregate(.~State,
+                     StatePop,
+                     sum)
+
 NationalDataTable = CovidConfirmedCases
 NationalDataTable$State = as.factor(NationalDataTable$State)
 NationalDataTable = NationalDataTable[,-c(1,2,4)]
-NationalDataTable = aggregate(.~State, NationalDataTable, sum)
+NationalDataTable = aggregate(.~State, 
+                              NationalDataTable, 
+                              sum)
+
+combNames = c(as.character(StatePop$State),as.character(NationalDataTable$State))
+dupNames = combNames[duplicated(combNames)]
+
+NationalDataTable = NationalDataTable %>% filter(State %in% dupNames)
+StatePop = StatePop %>% filter(State %in% dupNames)
+
 RateofCovidChange = rev(NationalDataTable)[c(1:7)]
 RateofCovidChange = ceiling(rowSums(RateofCovidChange[1:6]-RateofCovidChange[2:7])/6)
 
@@ -15,8 +32,11 @@ NationalDeathTable = aggregate(.~State,
                                NationalDeathTable, 
                                sum)
 
+NationalDeathTable = NationalDeathTable %>% filter(State %in% dupNames)
+
 RateofDeathChange  = rev(NationalDeathTable)[c(1:7)]
 RateofDeathChange  = ceiling(rowSums(RateofDeathChange[1:6]-RateofDeathChange[2:7])/6)
+
 
 NationalDataTable = data.frame(NationalDataTable$State,
                                NationalDataTable[,length(NationalDataTable)],
@@ -30,22 +50,7 @@ colnames(NationalDataTable) = c("State",
                                 "Total Deaths",
                                 "Average New Deaths Per Day")
 
-NationalDataTable$`Cases Per 100,000 People` = c(731545,4903185,3017825,
-                                                 7278717,39512223,5758736,
-                                                 3565287,705749,973764,
-                                                 21477737,10617423,1415872,
-                                                 3155070,1787065,12671821,
-                                                 6732219,2913314,4467673,
-                                                 4648794,6949503,6045680,
-                                                 1344212,9986857,5639632,
-                                                 6137428,2976149,1068778,
-                                                 10488084,762062,1934408,
-                                                 1359711,8882190,2096829,
-                                                 3080156,19453561,11689100,
-                                                 3956971,4217737,12801989,
-                                                 1059361,5148714,884659,
-                                                 6833174,28995881,3205958,
-                                                 8535519,623989,7614893,
-                                                 5822434,1792147,578759)
+
+NationalDataTable$`Cases Per 100,000 People` = as.numeric(StatePop$Population)
 
 NationalDataTable$`Cases Per 100,000 People` = round(NationalDataTable$`Total Cases`/(NationalDataTable$`Cases Per 100,000 People`/100000))
