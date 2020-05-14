6128dbfb513a5f36dfd9aaf5a77d055867f1ac81
tleonard6 <taylorleonard15@gmail.com>
2020-05-14 15:34:37 -0400

    Data Updates

--- covid19/www/4_load_external_data/12_AF_Wing.R
+++ covid19/www/4_load_external_data/12_AF_Wing.R
@@ -9,37 +9,38 @@
 # WingList <- c("All",WingList)
 
 # ##Constructing the cimdtest and himdtest matrices
-# AFBaseLocations2 = vroom::vroom("C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/AllServiceBases.csv")
-# 
+# AFBaseLocations2 = vroom::vroom("C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/AllBaseList.csv")
+# AFBaseLocations2<-data.frame(AFBaseLocations2) 
 # AFBaseLocations2$Lat <- as.numeric(AFBaseLocations2$Lat)
 # AFBaseLocations2$Long <- as.numeric(AFBaseLocations2$Long)
+#  
 # 
-# x<-data.frame(AFBaseLocations2$Long,AFBaseLocations2$Lat)
+# AFBaseLocations<-merge(AFBaseLocations2,CountyInfo, by.x = c("State2","County"), by.y = c("State","County"))
+# #AFBaseLocations<-AFBaseLocations[!duplicated(AFBaseLocations$'Location Name'), ]
+# #only keep the useful columns
+# AFBaseLocations<-data.frame(AFBaseLocations$'Location Name',AFBaseLocations$State2,
+#                              AFBaseLocations$County,AFBaseLocations$Country1,AFBaseLocations$Branch,
+#                              AFBaseLocations$Operational,AFBaseLocations$Lat,AFBaseLocations$Long,
+#                              AFBaseLocations$'Major Command',AFBaseLocations$FIPS)
+# 
+# colnames(AFBaseLocations)<-c("Base","State","County","Country","Branch","Operational","Lat","Long","Major Command","FIPS")
+# 
+# x<-data.frame(AFBaseLocations$Long,AFBaseLocations$Lat)
 # y<-data.frame(CountyInfo$Longitude,CountyInfo$Latitude)
 # cimd <- distm(y, x, fun=distHaversine)/1609.34
-# colnames(cimd)<- AFBaseLocations2$Base
+# colnames(cimd)<- AFBaseLocations$Base
 # 
-# x<-data.frame(AFBaseLocations2$Long,AFBaseLocations2$Lat)
+# x<-data.frame(AFBaseLocations$Long,AFBaseLocations$Lat)
 # y<-data.frame(HospitalInfo$LONGITUDE,HospitalInfo$LATITUDE)
 # himd <- distm(y, x, fun=distHaversine)/1609.34
-# colnames(himd)<- AFBaseLocations2$Base
+# colnames(himd)<- AFBaseLocations$Base
 # 
-# AFBaseLocations2<-data.frame(AFBaseLocations2)
-# AFBaseLocations<-merge(AFBaseLocations2,CountyInfo, by.x = c("State","County"), by.y = c("State","County"))
-# AFBaseLocations<-AFBaseLocations[!duplicated(AFBaseLocations$Base), ]
-# #only keep the useful columns
-# AFBaseLocations<-data.frame(AFBaseLocations$Base,AFBaseLocations$City,AFBaseLocations$State,
-#                              AFBaseLocations$County,AFBaseLocations$Country,AFBaseLocations$Branch,
-#                              AFBaseLocations$Operational,AFBaseLocations$Lat,AFBaseLocations$Long,
-#                              AFBaseLocations$Major.Command,AFBaseLocations$FIPS)
-# 
-# colnames(AFBaseLocations)<-c("Base","City","State","County","Country","Branch","Operational","Lat","Long","Major Command","FIPS")
-# 
-# write.csv(cimd,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/cimd.csv", row.names = FALSE)
-# write.csv(himd,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/himd.csv", row.names = FALSE)
-# write.csv(AFBaseLocations,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/AFBaseLocations.csv", row.names = FALSE)
-# write.csv(GlobalData,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/GlobalData.csv", row.names = FALSE)
-# 
+# write.csv(cimd,"C:/Users/taylo/Documents/CHAD/cimd.csv", row.names = FALSE)
+# write.csv(himd,"C:/Users/taylo/Documents/CHAD/himd.csv", row.names = FALSE)
+# write.csv(AFBaseLocations,"C:/Users/taylo/Documents/CHAD/AFBaseLocations.csv", row.names = FALSE)
+#write.csv(GlobalData,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/GlobalData.csv", row.names = FALSE)
+
+
 # setwd("C:/Users/taylo/Documents/CHAD/covid19/www/3_load_local_data/")
 # save(cimd, file = "cimd.rda")
 # save(himd, file = "himd.rda")
