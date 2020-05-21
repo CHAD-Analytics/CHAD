5a48ab9e4bfa3423f39330b138c090ab4606dff6
tleonard6 <taylorleonard15@gmail.com>
2020-05-21 14:51:30 -0400

    ArmyUpdate

--- covid19/www/4_load_external_data/14_Army_Model.R
+++ covid19/www/4_load_external_data/14_Army_Model.R
@@ -15,3 +15,6 @@ Army_Model$number <- Army_Model$number+1
 Army_Model$Infected <- Army_Model$Infected/Army_Model$number
 Army_Model$Fatalities <- Army_Model$Fatalities/Army_Model$number
 Army_Model<-separate_rows(Army_Model,FIPS,County,convert = TRUE)
+Army_Model$County<-as.character(Army_Model$County)
+Army_Model$Location<-as.character(Army_Model$Location)
+Army_Model$State<-as.character(Army_Model$State)
