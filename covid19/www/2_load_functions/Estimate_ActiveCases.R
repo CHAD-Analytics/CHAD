6e1497d01048c437b303769cbf8a39b7cf8874ce
galarcon0308 <52179539+galarcon0308@users.noreply.github.com>
2020-07-14 01:05:21 -0400

    Updated active case count

--- covid19/www/2_load_functions/Estimate_ActiveCases.R
+++ covid19/www/2_load_functions/Estimate_ActiveCases.R
@@ -35,22 +35,54 @@ Estimate_ActiveCases <- function(CONUSSelect,ChosenBase,IncludedCounties){
   
   
   #Find counties in radius
-  if (CONUSSelect == "CONUS"){
-      CovidCountiesCases<-subset(CovidActiveCases1, FIPS %in% IncludedCounties$FIPS)
-      #CovidCountiesCases %>% group_by(FIPS) %>% summarize(Last_Update = max(Last_Update))
-      AC <- CovidCountiesCases$Active  
-      AC <- sum(AC)
-  } else {
-      if (IncludedCounties$State == "HI" || IncludedCounties$State == "AK" || IncludedCounties$State == "PR") {
-        CovidCountiesCases<-subset(CovidActiveCases1, FIPS %in% IncludedCounties$FIPS)
-        AC <- CovidCountiesCases$Active  
-        AC <- sum(AC)        
-      } else {
-        CovidCountiesCases<-subset(GlobalActive, FIPS %in% IncludedCounties$FIPS) 
-        AC <- NA
-      }
-  }
+  # if (CONUSSelect == "CONUS"){
+  #     CovidCountiesCases<-subset(CovidActiveCases1, FIPS %in% IncludedCounties$FIPS)
+  #     #CovidCountiesCases %>% group_by(FIPS) %>% summarize(Last_Update = max(Last_Update))
+  #     AC <- CovidCountiesCases$Active  
+  #     AC <- sum(AC)
+  # } else {
+  #     if (IncludedCounties$State == "HI" || IncludedCounties$State == "AK" || IncludedCounties$State == "PR") {
+  #       CovidCountiesCases<-subset(CovidActiveCases1, FIPS %in% IncludedCounties$FIPS)
+  #       AC <- CovidCountiesCases$Active  
+  #       AC <- sum(AC)        
+  #     } else {
+  #       CovidCountiesCases<-subset(GlobalActive, FIPS %in% IncludedCounties$FIPS) 
+  #       AC <- NA
+  #     }
+  # }
+  # 
+  # Final <- AC
   
-  Final <- AC
+  
+  
+  # Get recovered, total and death number first
+  #Find counties in radius
+  CovidCountiesCases<-subset(CovidConfirmedCases, CountyFIPS %in% IncludedCounties$FIPS)
+  CovidCountiesDeath<-subset(CovidDeaths, CountyFIPS %in% IncludedCounties$FIPS)
+  
+  #Compute cumlative cases and deaths in selected counties
+  CumCovid<-colSums(CovidCountiesCases[,5:length(CovidCountiesCases)])
+  CumDeaths<-colSums(CovidCountiesDeath[5:length(CovidCountiesDeath)])
+  
+  #Estimate recovered from cases and deaths
+  DailyCases = CumCovid[2:length(CumCovid)] - CumCovid[1:(length(CumCovid)-1)]
+  DailyDeaths = CumDeaths[2:length(CumDeaths)] - CumDeaths[1:(length(CumDeaths)-1)]
+  DailyRecovered = DailyCases[1:(length(CumCovid)-14)] - DailyDeaths[5:(length(CumDeaths)-9)]
+  CumRecovered = cumsum(DailyRecovered)
+  cumcount<-nrow(as.data.frame(CumRecovered))
+  CRec <- CumRecovered[cumcount]
+  CRec <- substr(CRec,1,9)
+  CRec <- as.numeric(CRec)
+  
+  # Totals
+  TotCases = rev(CumCovid)[1] 
+  TotDeaths = rev(CumDeaths)[1]
+  
+  # Active
+  AC = TotCases - TotDeaths - CRec
   
 }
+  
+  
+  
+
