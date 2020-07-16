779a97365a8ec5bca837671b6a3b88a215f0d12c
galarcon0308 <52179539+galarcon0308@users.noreply.github.com>
2020-07-16 13:41:49 -0400

    Updates to base file and distance matrices

--- covid19/www/4_load_external_data/12_AF_Wing.R
+++ covid19/www/4_load_external_data/12_AF_Wing.R
@@ -22,28 +22,26 @@
 #                              AFBaseLocations$Operational,AFBaseLocations$Lat,AFBaseLocations$Long,
 #                              AFBaseLocations$'Major.Command',AFBaseLocations$FIPS)
 # colnames(AFBaseLocations)<-c("ID","Base","State","County","Country","Branch","Operational","Lat","Long","Major Command","FIPS")
-
-# AFBaseLocations2<-AFBaseLocations[!duplicated(AFBaseLocations$'Base'), ]
 # 
-# x<-data.frame(AFBaseLocations2$Long,AFBaseLocations2$Lat)
+# AFBaseLocations<-AFBaseLocations[!duplicated(AFBaseLocations$'Base'), ]
+# 
+# x<-data.frame(AFBaseLocations$Long,AFBaseLocations$Lat)
 # y<-data.frame(CountyInfo$Longitude,CountyInfo$Latitude)
 # cimd <- distm(y, x, fun=distHaversine)/1609.34
-# colnames(cimd)<- AFBaseLocations2$Base
+# cimd = data.frame(cimd)
+# colnames(cimd)<- AFBaseLocations$Base
 # 
-# x<-data.frame(AFBaseLocations2$Long,AFBaseLocations2$Lat)
+# x<-data.frame(AFBaseLocations$Long,AFBaseLocations$Lat)
 # y<-data.frame(HospitalInfo$LONGITUDE,HospitalInfo$LATITUDE)
 # himd <- distm(y, x, fun=distHaversine)/1609.34
-# colnames(himd)<- AFBaseLocations2$Base
+# himd = data.frame(himd)
+# colnames(himd)<- AFBaseLocations$Base
 # 
-# write.csv(cimd,"C:/Users/taylo/Documents/CHAD/cimd.csv", row.names = FALSE)
-# write.csv(himd,"C:/Users/taylo/Documents/CHAD/himd.csv", row.names = FALSE)
-# write.csv(AFBaseLocations,"C:/Users/taylo/Documents/CHAD/AFBaseLocations.csv", row.names = FALSE)
-#write.csv(GlobalData,"C:/Users/taylo/Documents/CHAD/covid19/www/4_load_external_data/data_files/GlobalData.csv", row.names = FALSE)
-
-
-# setwd("C:/Users/taylo/Documents/CHAD/covid19/www/3_load_local_data/")
-# save(cimd, file = "cimd.rda")
-# save(himd, file = "himd.rda")
+# 
+# 
+# # setwd("C:/Users/taylo/Documents/CHAD/covid19/www/3_load_local_data/")
+# save(cimd, file = "www/3_load_local_data/cimd.rda")
+# save(himd, file = "www/3_load_local_data/himd.rda")
 # save(AFBaseLocations, file = "AFBaseLocations.rda")
 # save(AFNAFS, file = "AFNAS.rda")
 # cimd<-vroom::vroom("www/4_load_external_data/data_files/cimd.csv")
