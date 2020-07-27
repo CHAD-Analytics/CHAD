2c9e8250271bd9224b73f9b8f48c0b38fe24f5d7
galarcon0308 <52179539+galarcon0308@users.noreply.github.com>
2020-07-26 21:40:23 -0400

    Update Army model read in

--- covid19/www/4_load_external_data/14_Army_Model.R
+++ covid19/www/4_load_external_data/14_Army_Model.R
@@ -1,18 +1,20 @@
 ### Have to rerun with new Army files
 
-Army_Model = read_csv("www/4_load_external_data/data_files/para_R_abridged.csv")
-colset<-c(2,3,4,5,6,7,8,9,10,11,17,18,19,20,21,22,23,24,25,26,27,28)
-Army_Model<-Army_Model[, names(Army_Model)[colset]]
-colnames(Army_Model)<-c("ForecastDate","FIPS","County","StateFull","Susceptible","Exposed","Infected","Removed","Fatalities","TFatalities"
-                                                     ,"LSusceptible","LExposed","LInfected","LRemoved","LFatalities","LTFatalities"
-                                                     ,"USusceptible","UExposed","UInfected","URemoved","UFatalities","UTFatalities")
+# Army_Model = read_csv("www/4_load_external_data/data_files/para_R_abridged.csv")
+# colset<-c(2,3,4,5,6,7,8,9,10,11,17,18,19,20,21,22,23,24,25,26,27,28)
+# Army_Model<-Army_Model[, names(Army_Model)[colset]]
+# colnames(Army_Model)<-c("ForecastDate","FIPS","County","StateFull","Susceptible","Exposed","Infected","Removed","Fatalities","TFatalities"
+                                                     # ,"LSusceptible","LExposed","LInfected","LRemoved","LFatalities","LTFatalities"
+                                                     # ,"USusceptible","UExposed","UInfected","URemoved","UFatalities","UTFatalities")
 
 Army_Model <- merge(Army_Model,
                      CountyInfo,
-                     by.x = names(Army_Model[2]),
+                     by.x = names(Army_Model[3]),
                      by.y = names(CountyInfo)[3])
 
-Army_Model<-subset(Army_Model, select=-c(22,24,25,26,27,28,29,30,31,32))
+# Army_Model<-subset(Army_Model, select=-c(22,24,25,26,27,28,29,30,31,32))
+Army_Model<-subset(Army_Model, select= -c(15,16,17,18,19,20,21,22,23,24,25))
 Army_Model$FIPS <- as.numeric(Army_Model$FIPS)
 Army_Model$ForecastDate <- as.Date(Army_Model$ForecastDate, "%m/%d/%Y")
 #save(Army_Model, file = "Army_Model.rda")
+
